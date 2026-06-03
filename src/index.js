89bccdc5e9c8ad179af3a8c8e71e8796fa62b46f
Fabricio <fabricio.bizotto@ifc.edu.br>
2026-06-03 08:29:09 -0300

    test

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/soma/:a/:b', (req, res) => {
-    const resultado = Number(req.params.a) + Number(req.params.b);
-    // const resultado = Number(req.params.a) - Number(req.params.b);
+    // const resultado = Number(req.params.a) + Number(req.params.b);
+    const resultado = Number(req.params.a) - Number(req.params.b);
     res.json({ resultado });
 });
 
